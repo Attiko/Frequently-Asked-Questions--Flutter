7ee9efadffbbb60d3d7ac7d9e4d95a145939af55
attikoo@uni.coventry.ac.uk <attikoo@coventry.ac.uk>
2022-03-26 13:15:00 +0000

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,253 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(LoginPage());
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  int activeIndex = 0;
+  Future<String> getData() async {
+    var response =
+        await http.post(Uri.parse("http://localhost:8080/questions"));
+    print(response.body);
+    setState(() {
+      data = jsonDecode(response.body);
+    });
+    return "congrat";
+  }
+
+  @override
+  void initState() {
+    Timer.periodic(Duration(seconds: 5), (timer) {
+      setState(() {
+        activeIndex++;
+
+        if (activeIndex == 4) activeIndex = 0;
+      });
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  height: 350,
+                  child: Stack(children: [
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                          opacity: activeIndex == 0 ? 1 : 0,
+                          duration: Duration(
+                            seconds: 1,
+                          ),
+                          curve: Curves.linear,
+                          child: Image.asset('images/log.png')),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                          opacity: activeIndex == 1 ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: Image.asset('images/unlock.png')),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                          opacity: activeIndex == 2 ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: Image.asset('images/pinfo.png')),
+                    ),
+                    Positioned(
+                      top: 0,
+                      left: 0,
+                      right: 0,
+                      bottom: 0,
+                      child: AnimatedOpacity(
+                          opacity: activeIndex == 3 ? 1 : 0,
+                          duration: Duration(seconds: 1),
+                          curve: Curves.linear,
+                          child: Image.asset('images/welcome.png')),
+                    )
+                  ]),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0.0),
+                    labelText: 'Username',
+                    hintText: 'Username',
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                    prefixIcon: Icon(
+                      Iconsax.user,
+                      color: Colors.purple,
+                      size: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.grey.shade200, width: 2),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  cursorColor: Colors.black,
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.all(0.0),
+                    labelText: 'Password',
+                    hintText: 'Password',
+                    hintStyle: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400,
+                    ),
+                    prefixIcon: Icon(
+                      Iconsax.key,
+                      color: Colors.purple,
+                      size: 18,
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.grey.shade200, width: 2),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    floatingLabelStyle: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18.0,
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 1.5),
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                MaterialButton(
+                  onPressed: () async {
+                    var response = await http.post(
+                        Uri.parse('http/localhost:8080/users'),
+                        body: {'name': 'doodle', 'color': 'blue'});
+                  },
+                  height: 45,
+                  color: Colors.black,
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white, fontSize: 16.0),
+                  ),
+                  padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(
+                          color: Colors.grey.shade600,
+                          fontSize: 14.0,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Register',
+                        style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
